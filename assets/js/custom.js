1824239b5ec0d50ed7d994c4a572004fcebd37d9
Sanjeev Kumar <sanjvsingla@gmail.com>
2020-03-12 12:39:49 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$.noConflict();
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
