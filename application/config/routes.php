0421c36502d64a5eedd1c489f386719689bfce95
Sanjeev Kumar <sanjvsingla@gmail.com>
2020-03-31 16:08:39 +0530

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,16 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*   To Load the forms     */
-$route['default_controller'] = 'user/register';
-$route['login'] = 'user/login';
-$route['reset-forgot-password'] = 'user/reset_forgot_password';
+$route['default_controller'] = 'common/user_register';
+$route['login'] = 'common/user_login';
+$route['reset-forgot-password'] = 'common/reset_forgot_password';
 
 /*  To call the controller and functions   */
-$route['register'] = 'users/common/user_register';
-$route['signin'] = 'users/common/user_login';
-$route['forgot'] = 'users/common/forgot';
+$route['register'] = 'common/user_register';
+$route['signin'] = 'common/user_login';
+$route['forgot'] = 'common/forgot';
 $route['translate_uri_dashes'] = FALSE;
-$route['reset-password'] = 'users/common/reset_password_update';
+$route['reset-password'] = 'common/reset_password_update';
 
 /* Admin URLS*/
 $route['admin'] = "admin/admin";
